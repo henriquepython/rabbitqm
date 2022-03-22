dadbfbeade9b27eca271ae6bca3cd912039016b0
Henriquepython <joaohbp59@gmail.com>
2022-03-22 17:09:16 -0300

    first commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,58 @@
+﻿using Consumer.Domain;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Text.Json;
+
+namespace Consumer
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "orderQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                //consumo
+                var consumer = new EventingBasicConsumer(channel);
+                
+                //evento de recepção
+                consumer.Received += (model, ea) =>
+                {
+                    try 
+                    { 
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+
+                        //converter string em objeto de volta
+                        var order = JsonSerializer.Deserialize<Order>(message);
+
+                        Console.WriteLine($" [x] Order: {order.OrderNumber}|{order.ItemName}|{order.Price:N2}", message);
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch (Exception ex)
+                    {
+                        //Logger,, Nack é não ok.
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                };
+                channel.BasicConsume(queue: "orderQueue",
+                                     autoAck: false, //colocar autoAck em false, pois se der problema no work,  voce consumiu, vc perde a mensagem
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ rabbitqm/Controllers/OrderController.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using rabbitqm.Domain;
+
+namespace rabbitqm.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult InsertOrder(Order order)
+        {
+            try
+            {
+                //rabbitMQ producer
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    //alterar queue para nome da fila ex "orderQueue"
+                    channel.QueueDeclare(queue: "orderQueue",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    //pega o pedido e converte ele no formato texto como mensagem
+                    string message = JsonSerializer.Serialize(order);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "orderQueue",//colocar o mesmo nome da queue
+                                         basicProperties: null,
+                                         body: body);
+                }
+                //retorna Accepted 202
+                return Accepted(order);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Erro ao tentar criar um novo pedido", ex);
+
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+}
